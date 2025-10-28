f0d9d65ed11411792c214ee838a49403c6782ff0
mjhimran <jahidh9991@gmail.com>
2025-10-29 03:55:18 +0600

    small change

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 text = 'hello world'
 
-import random
-r = random.randint(1, 100)
+# import random
+# r = random.randint(1, 100)
 
 def main():
-    open(f'output{r}.txt', 'w').write(text)
+    with open('output.txt', 'r') as file:
+        content = file.read()
+
+    content += f'\n{text}'
+    open(f'output.txt', 'w').write(content)
 
 if __name__ == '__main__':
     main()
